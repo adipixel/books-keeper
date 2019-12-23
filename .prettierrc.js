bcdcfdf378a0b178744773d48f5d7ef6bffda8dc
Aditya Mhamunkar <amhamunkar@lastline.com>
2019-12-23 11:30:11 -0800

    feat: Add and apply preiiter

---
+++ .prettierrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  // Include parentheses around a sole arrow function parameter. (default: "avoid")
+  arrowParens: "always",
+
+  // By default, Prettier preserves a flavor of line endings a given file has already used. It also converts mixed line endings within one file to what it finds at the end of the first line.
+  // Valid options: "auto", "lf", "crlf", "cr" (default: "auto")
+  endOfLine: "lf",
+
+  // Using the JSON parser for JSON files
+  overrides: [
+    {
+      files: "*.json",
+      options: {
+        parser: "json"
+      }
+    },
+    {
+      files: ["*.ts", "*.tsx"],
+      options: {
+        parser: "typescript"
+      }
+    }
+  ],
+
+  // Print semicolons at the ends of statements. (default: true)
+  semi: true,
+
+  // Use single quotes instead of double quotes. (default: false)
+  singleQuote: true,
+
+  // Print trailing commas wherever possible when multi-line. (default: "none")
+  trailingComma: "es5"
+};
